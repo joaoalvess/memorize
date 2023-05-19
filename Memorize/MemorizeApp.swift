5e846d8f6ff153d3b3c99ec7bd84240679317594
joaoalvess <contato@joaoalvescosta.com.br>
2023-05-19 15:24:06 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Joao Alves on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
